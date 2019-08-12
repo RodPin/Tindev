249402341802b625013235c490e656deb9375dbd
RODRIGO <prodrigo@poli.ufrj.br>
2019-08-12 14:31:07 -0300

    routing

--- webfrontend/src/App.js
+++ webfrontend/src/App.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import './App.css';
-import Login from './pages/Login'
+import Routes from './routes';
 function App() {
-  return (
-    <Login/>
-  );
+  return <Routes />;
 }
 
 export default App;
