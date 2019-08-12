fe307eebe88e874469c1e0305627c90738a1a80e
RODRIGO <prodrigo@poli.ufrj.br>
2019-08-12 15:38:12 -0300

    fetching users

--- webfrontend/src/pages/Main.js
+++ webfrontend/src/pages/Main.js
@@ -5,85 +5,40 @@ import like from '../assets/like.svg';
 import dislike from '../assets/dislike.svg';
 import api from '../services/api';
 export default function Main({match}) {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    async function loadUsers() {
+      try {
+        const response = await api.get('/devs', {headers: {user: match.params.id}});
+        setUsers(response.data);
+      } catch (e) {
+        console.log(e);
+      }
+    }
+    loadUsers();
+  }, [match.params.id]);
+
   return (
     <div className="main-container">
       <img src={logo} alt="Tindev" />
       <ul>
-        <li>
-          <img src="https://avatars2.githubusercontent.com/u/861751?v=4" alt="" />
-          <footer>
-            <strong>Ronaldo Fenomeno</strong>
-            <p>Jogador de Futebol</p>
-          </footer>
-          <div className="buttons">
-            <button type="button">
-              <img src={dislike} alt="dislike" />
-            </button>
-            <button type="button">
-              <img src={like} alt="like" />
-            </button>
-          </div>
-        </li>
-        <li>
-          <img src="https://avatars2.githubusercontent.com/u/861751?v=4" alt="" />
-          <footer>
-            <strong>Ronaldo Fenomeno</strong>
-            <p>Jogador de Futebol</p>
-          </footer>
-          <div className="buttons">
-            <button type="button">
-              <img src={dislike} alt="dislike" />
-            </button>
-            <button type="button">
-              <img src={like} alt="like" />
-            </button>
-          </div>
-        </li>
-        <li>
-          <img src="https://avatars2.githubusercontent.com/u/861751?v=4" alt="" />
-          <footer>
-            <strong>Ronaldo Fenomeno</strong>
-            <p>Jogador de Futebol</p>
-          </footer>
-          <div className="buttons">
-            <button type="button">
-              <img src={dislike} alt="dislike" />
-            </button>
-            <button type="button">
-              <img src={like} alt="like" />
-            </button>
-          </div>
-        </li>
-        <li>
-          <img src="https://avatars2.githubusercontent.com/u/861751?v=4" alt="" />
-          <footer>
-            <strong>Ronaldo Fenomeno</strong>
-            <p>Jogador de Futebol</p>
-          </footer>
-          <div className="buttons">
-            <button type="button">
-              <img src={dislike} alt="dislike" />
-            </button>
-            <button type="button">
-              <img src={like} alt="like" />
-            </button>
-          </div>
-        </li>
-        <li>
-          <img src="https://avatars2.githubusercontent.com/u/861751?v=4" alt="" />
-          <footer>
-            <strong>Ronaldo Fenomeno</strong>
-            <p>Jogador de Futebol</p>
-          </footer>
-          <div className="buttons">
-            <button type="button">
-              <img src={dislike} alt="dislike" />
-            </button>
-            <button type="button">
-              <img src={like} alt="like" />
-            </button>
-          </div>
-        </li>
+        {users.map(user => (
+          <li key={user._id}>
+            <img src={user.avatar} alt="" />
+            <footer>
+              <strong>{user.name}</strong>
+              <p>{user.bio}</p>
+            </footer>
+            <div className="buttons">
+              <button type="button">
+                <img src={dislike} alt="dislike" />
+              </button>
+              <button type="button">
+                <img src={like} alt="like" />
+              </button>
+            </div>
+          </li>
+        ))}
       </ul>
     </div>
   );
