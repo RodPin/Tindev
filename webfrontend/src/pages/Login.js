5389fa041d414487aeb8e386188780fd01511369
RODRIGO <prodrigo@poli.ufrj.br>
2019-08-12 14:25:13 -0300

    Login page webFrontEnd

---
+++ webfrontend/src/pages/Login.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import logo from '../assets/logo.svg'
+import './Login.css'
+
+export default function Login(){
+    return(
+        <div className='login-container'>
+
+            <form>
+                <img src={logo} alt='Tindev'/>    
+                <input
+                    placeholder='Digite seu Github'
+                />
+                <button type='submit'>Enviar</button>
+            </form>
+        </div>
+    )
+}
