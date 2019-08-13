bbe73d96f3c2a7ce103281ecb09c97f593a93f98
RODRIGO <prodrigo@poli.ufrj.br>
2019-08-13 13:30:27 -0300

    socket.io to backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,15 +4,21 @@ const routes = require('./routes');
 //cors => make acessible to other frameworks
 const cors = require('cors');
 //creating a server with express
+const httpServer = express();
+//mixing socket and http server
+const server = require('http').Server(httpServer);
+const io = require('socket.io')(server);
 
-const server = express();
+io.on('connection', socket => {
+  console.log('New connectino', socket.id);
+});
 
 mongoose.connect('mongodb+srv://Rodrigo:senha123@omni8-ovtmr.mongodb.net/omni8?retryWrites=true&w=majority', {
   useNewUrlParser: true
 });
 
-server.use(cors());
-server.use(express.json());
-server.use(routes);
+httpServer.use(cors());
+httpServer.use(express.json());
+httpServer.use(routes);
 
 server.listen(3333);
